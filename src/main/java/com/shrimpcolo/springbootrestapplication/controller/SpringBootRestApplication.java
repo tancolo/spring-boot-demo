cd34b08b96ae011c4aeab89b918fa6e2fc9900db
shrimpcolo <shrimpcolo@gmail.com>
2018-01-14 22:39:30 +0800

    add JPA for database

--- src/main/java/com/shrimpcolo/springbootrestapplication/controller/SpringBootRestApplication.java
+++ src/main/java/com/shrimpcolo/springbootrestapplication/controller/SpringBootRestApplication.java
@@ -2,13 +2,17 @@ package com.shrimpcolo.springbootrestapplication.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @ComponentScan("com.shrimpcolo.springbootrestapplication")
+@EnableJpaRepositories("com.shrimpcolo.springbootrestapplication.jpa")
+@EntityScan("com.shrimpcolo.springbootrestapplication.jpa")
 public class SpringBootRestApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/shrimpcolo/springbootrestapplication/jpa/User.java
@@ -0,0 +1,51 @@
+package com.shrimpcolo.springbootrestapplication.jpa;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String role;
+
+    protected User() {
+    }
+
+    public User(String name, String role) {
+        super();
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
